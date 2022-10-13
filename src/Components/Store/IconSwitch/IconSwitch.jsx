731aa998c62765db6fbdf544deb3feb8b3185f6b
glebfomin28 <gleb.fom28@gmail.com>
2022-10-13 19:42:08 +0500

    task_2
    task_2

---
+++ src/Components/Store/IconSwitch/IconSwitch.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import "./IconSwitch.css";
+
+export const IconSwitch = (props) => {
+    const [flag, setFlag] = useState(2)
+
+    const onSwitch = () => {
+        setFlag(flag + 1)
+        props.onSwitch(flag)
+
+    }
+
+    return (
+        <div className="iconContainer">
+            <img onClick={onSwitch} className="icon" src={props.icon} alt="icon"/>
+        </div>
+    );
+}
